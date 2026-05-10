337e8a0b0597e2150bbe40c36a0925d8fb416a25
michelledelossantos1067 <michelledelossantos1067@gmail.com>
2026-05-10 17:53:42 -0400

    feat: agrega capa de servicios y lógica de negocio

---
+++ VentaControl.Services/ProductoService.cs
@@ -0,0 +1,58 @@
+using VentaControl.Services.interfaces;
+using VentaControl.Models;
+using VentaControl.Models.Dto;
+using VentaControl.Repositories;
+using VentaControl.Repositories.interfaces;
+
+namespace VentaControl.Services;
+
+public class ProductoService : IProductoService{
+    private readonly IProductosRepositories _repository;
+
+    public ProductoService(IProductosRepositories repository){
+        _repository = repository;
+    }
+    public async Task<List<Producto>> ObtenerTodos(){
+        
+        return await _repository.ObtenerTodos();
+    }
+    public async Task<Producto> ObtenerPorId(int Id){
+        
+        return await _repository.ObtenerPorId(Id);
+    }
+
+    public async Task Crear(ProductoDto ProductoDto){
+        if (ProductoDto.Precio < 0){
+            throw new Exception("El precio debe ser mayor que 0");
+        };
+        var producto = new Producto{
+            Nombre = ProductoDto.Nombre,
+            Descripcion = ProductoDto.Descripcion,
+            Precio = ProductoDto.Precio,
+            Stock = ProductoDto.Stock,
+        };
+        await _repository.Crear(producto);
+    }
+    public async Task Actualizar(int Id,ProductoDto productoDto){
+
+        var producto = await _repository.ObtenerPorId(Id);
+        if (producto == null){
+            throw new Exception("Producto no encontrado.");
+        };
+
+        producto.Nombre = productoDto.Nombre;
+        producto.Descripcion = productoDto.Descripcion;
+        producto.Precio = productoDto.Precio;
+        producto.Stock = productoDto.Stock;
+
+        await _repository.Actualizar(Id,producto);
+    }
+    public async Task Eliminar(int Id){
+        var producto = await _repository.ObtenerPorId(Id);
+        
+        if (producto == null){
+            throw new Exception("El producto no existe.");
+        };
+        await _repository.Eliminar(Id);
+    }
+}
